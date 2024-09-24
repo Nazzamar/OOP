870dd0e2c814fa512b32eb4092b885dae8f649c6
Кантаев Рамазан Григорьевич <iv322s13@csc.sibsutis.ru>
2024-09-24 22:38:11 +0700

    comlete 1lab

--- functions.cpp
+++ functions.cpp
@@ -1,40 +1,35 @@
 #include "functions.hpp"
 
-int* genRandArray(int size, int maxValue)
-{
-    int* arr = new int[size+1];
-    *arr = size;
-    for( int i = 1; i<size+1; i++){
-        *(arr+i) = rand()%maxValue;
-    }
-    return arr;
+void print(int *arr) {
+  int size = *arr;
+  std::cout << *arr << ": ";
+  for (int i = 1; i < size + 1; i++) {
+    std::cout << std::setw(3) << *(arr + i);
+  }
+  std::cout << "\n";
 }
-void print(int* arr)
-{
-    int size = *arr;
-    std::cout << *arr << ": ";
-    for(int i = 1; i<size+1; i++){
-        std::cout << std::setw(4) <<*(arr + i);
-    }
+int *genRandArray(int size, int maxValue) {
+  int *arr = new int[size + 1];
+  *arr = size;
+  for (int i = 1; i < *arr + 1; i++) {
+    *(arr + i) = rand() % maxValue;
+  }
+  return arr;
 }
 
-
-int** genRandMatrix(int size, int maxValue)
-{
-    int **matrix = new int* [size+1];
-    **matrix = size;
-    for(int i = 1; i < size+1; i++){
-        size = rand()%10;
-        *(matrix+i) = genRandArray(size, maxValue);
-    }
+int **genRandMatrix(int size, int maxValue) {
+  int **matrix = new int *[size + 1];
+  *matrix = &size;
+  for (int i = 1; i < (**matrix) + 1; i++) {
+    int arrsize = rand() % 10;
+    *(matrix + i) = genRandArray(arrsize, maxValue);
+  }
+  return matrix;
 }
-void printMatrix(int** matrix)
-{
-    std::cout << **matrix << "\n";
-    for(int i = 1; i < **matrix+1; i++){
-        for(int j = 1; j < **(matrix+); j++){
-            std::cout << std::setw(1) << matrix[i][j];
-        }
-    }
-
+void printMatrix(int **matrix) {
+  int size = **matrix;
+  std::cout << (**matrix) << std::endl;
+  for (int i = 1; i < size + 1; i++) {
+    print(*(matrix + i));
+  }
 }
--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include "functions.hpp"
 
-int main()
-{   
-    srand(time(0));
-    int size = rand()%10;
-    int maxValue = 100;
-    // int* arr = genRandArray(size, maxValue);
-    // print(arr);
-    // delete [] arr;
+int main() {
+  srand(time(0));
+  int size = rand() % 10;
+  int maxValue = 100;
+  int *arr = genRandArray(size, maxValue);
+  print(arr);
+  delete[] arr;
 
-    int** matrix = genRandMatrix(size, maxValue);
-    printMatrix(matrix);
-    delete [] matrix[size];
+  int **matrix = genRandMatrix(size, maxValue);
+  printMatrix(matrix);
+  delete[] matrix[size + 1];
 }
