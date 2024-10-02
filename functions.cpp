f9bac09cf5f5b72efced5fe0404ae98476188fe1
Кантаев Рамазан Григорьевич <iv322s13@csc.sibsutis.ru>
2024-10-02 12:37:05 +0700

    nanimo nakatta

--- functions.cpp
+++ functions.cpp
@@ -2,9 +2,12 @@
 
 void print(int *arr) {
   int size = *arr;
-  std::cout << *arr << ": ";
-  for (int i = 1; i < size + 1; i++) {
-    std::cout << std::setw(3) << *(arr + i);
+  for (int i = 0; i < size + 1; i++) {
+    if (i == 0) {
+      std::cout << size << ": ";
+    } else {
+      std::cout << std::setw(3) << *(arr + i);
+    }
   }
   std::cout << "\n";
 }
@@ -19,16 +22,20 @@ int *genRandArray(int size, int maxValue) {
 
 int **genRandMatrix(int size, int maxValue) {
   int **matrix = new int *[size + 1];
-  *matrix = &size;
-  for (int i = 1; i < (**matrix) + 1; i++) {
+  for (int i = 0; i < size + 1; i++) {
     int arrsize = rand() % 10;
-    *(matrix + i) = genRandArray(arrsize, maxValue);
+    if (i == 0) {
+      *matrix = new int[1];
+      **matrix = size;
+    } else {
+      *(matrix + i) = genRandArray(arrsize, maxValue);
+    }
   }
   return matrix;
 }
 void printMatrix(int **matrix) {
   int size = **matrix;
-  std::cout << (**matrix) << std::endl;
+  std::cout << (size) << std::endl;
   for (int i = 1; i < size + 1; i++) {
     print(*(matrix + i));
   }
--- main.cpp
+++ main.cpp
@@ -4,11 +4,14 @@ int main() {
   srand(time(0));
   int size = rand() % 10;
   int maxValue = 100;
-  int *arr = genRandArray(size, maxValue);
-  print(arr);
-  delete[] arr;
+  // int *arr = genRandArray(size, maxValue);
+  // print(arr);
+  // delete[] arr;
 
   int **matrix = genRandMatrix(size, maxValue);
   printMatrix(matrix);
-  delete[] matrix[size + 1];
+  for (int i = 0; i < size; i++) {
+    delete[] matrix[i];
+  }
+  delete[] matrix;
 }
